4b9e6a01be7267e0c2dd83675f09269fdada7627
Pape Khole DIOKH <diokh@192.168.1.171>
2026-04-16 14:24:51 +0000

    Add automatic superuser creation

--- build.sh
+++ build.sh
@@ -2,4 +2,8 @@
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+# Crée un superutilisateur automatiquement s'il n'existe pas
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin123') if not User.objects.filter(username='admin').exists() else None" | python manage.py shell
+
 echo "✅ Build terminé!"
