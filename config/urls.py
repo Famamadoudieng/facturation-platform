6c28703929ecc102f59ab9252cf527ec75d2b3a2
Pape Khole DIOKH <diokh@192.168.1.171>
2026-04-16 15:52:30 +0000

    Fix media files serving with whitenoise

--- config/urls.py
+++ config/urls.py
@@ -30,5 +30,10 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
     path('parametres/', include('parametres.urls')),
     path('paiements/', include('paiements.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if not settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
