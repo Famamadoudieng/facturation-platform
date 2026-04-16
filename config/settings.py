7011f11c08d8c54647d08a11bb5547c7ce8db886
Pape Khole DIOKH <diokh@192.168.1.171>
2026-04-16 15:59:05 +0000

    Fix media files serving with whitenoise

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,13 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-temp-key-for-build')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 #ALLOWED_HOSTS = []
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
+#ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'facturation-platform.onrender.com',
+    '.onrender.com',  # ✅ Permet tous les sous-domaines Render
+]
 
 
 # Application definition
